35c9ac8a9ecf8c0c408f61e00339b505a9722042
LANA.BI <LANA.BI@OOCL.COM>
2025-09-15 10:51:13 +0800

    feat: add defaultlayout

--- src/App.js
+++ src/App.js
@@ -4,6 +4,40 @@ import TodoList from "./components/TodoList";
 import {initialState, todoReducer} from "./reducers/todoReducer";
 import {TodoContext} from "./contexts/TodoContext";
 import {AddBar} from "./components/AddBar";
+import {createBrowserRouter, NavLink, Outlet, RouterProvider} from "react-router";
+
+function DefaultLayout() {
+    return <>
+        <header>
+            <nav>
+                <ul>
+                    <li><NavLink to={'/'}>Home</NavLink></li>
+                    <li>todoList</li>
+                    <li><NavLink to={'about'}>About</NavLink></li>
+                </ul>
+            </nav>
+        </header>
+        <main>
+            <h1>xxx</h1>
+            <Outlet></Outlet>
+        </main>
+        <footer>copyright</footer>
+    </>;
+}
+
+const routes=[{
+    path:'/',
+    element:<DefaultLayout/>,
+    children:[{
+        path:'/',
+        element:<h1>Home page</h1>,
+    },
+        {
+            path:'about',
+            element: <h1>About Us</h1>
+        }]
+}];
+const router=createBrowserRouter(routes);
 
 function App() {
     // the Hooks API manage component data state
@@ -14,11 +48,12 @@ function App() {
     }
     return (
         <div className="App">
-            <h1>Todo List</h1>
-            <TodoContext.Provider value={value}>
-                <TodoList/>
-            </TodoContext.Provider>
-            <AddBar onChange={toggleAdd}></AddBar>
+            <RouterProvider router={router}></RouterProvider>
+            {/*<h1>Todo List</h1>*/}
+            {/*<TodoContext.Provider value={value}>*/}
+            {/*    <TodoList/>*/}
+            {/*</TodoContext.Provider>*/}
+            {/*<AddBar onChange={toggleAdd}></AddBar>*/}
         </div>
     );
 }
